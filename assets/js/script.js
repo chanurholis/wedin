61af9165b2f633a0a38008c304ec1f1dc651a870
Chacha Nurholis <chachanurholis29@gmail.com>
2024-01-04 01:44:59 +0700

    improve: style

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,5 @@ const queryString = window.location.search;
 const params = new URLSearchParams(queryString);
 const nameParam = params.get('name');
 
+
 document.querySelector('.yts').textContent = nameParam ? `  ${nameParam}  ` : '  Everyone  ';
